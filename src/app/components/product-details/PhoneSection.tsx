cc2a884c5c5d6edd3256a3fcbf3db350d22fa65f
maga0721 <mahesh.garade@netcracker.com>
2025-03-15 22:00:54 +0530

    image size updated

--- src/app/components/product-details/PhoneSection.tsx
+++ src/app/components/product-details/PhoneSection.tsx
@@ -27,81 +27,92 @@ const PhoneSection: React.FC<PhoneSectionProps> = ({ data }) => {
   } = data;
   console.log("PhoneSection skuCode", skuCode);
   return (
-    <div
-      style={{
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "space-between",
-        position: "relative",
-        height: "500px",
-        width: "100%",
-        backgroundImage: `url(${backgroundImages})`,
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-      }}
-    >
-      {/* Phone Image on the Left */}
+    <div>
       <div
         style={{
-          position: "absolute",
-          top: "50%",
-          left: "10%",
-          transform: "translateY(-50%)",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+          position: "relative",
+          height: "250px",
+          width: "100%",
+          backgroundImage: `url(${backgroundImages})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
         }}
       >
-        <Image
-          src={image}
-          alt={`${brand} ${name}`}
-          width={200}
-          height={400}
-          style={{
-            objectFit: "contain",
-          }}
-        />
-      </div>
-
-      {/* Text and Review Details on the Right */}
-      <div
-        style={{
-          flex: "1",
-          color: "white",
-          padding: "20px",
-          marginLeft: "auto",
-          textAlign: "left",
-          position: "absolute",
-          right: "10%",
-        }}
-      >
-        <h1 style={{ margin: 0, fontSize: "24px" }}>{brand}</h1>
-        <h2 style={{ margin: "10px 0" }}>{name}</h2>
-        <button
-          style={{
-            backgroundColor: "#0096FF",
-            color: "white",
-            border: "none",
-            padding: "10px 20px",
-            borderRadius: "5px",
-            cursor: "pointer",
-          }}
-        >
-          {networkTechnology}
-        </button>
+        {/* Phone Image on the Left */}
         <div
           style={{
-            display: "flex",
-            alignItems: "center",
-            marginTop: "10px",
-            fontSize: "16px",
+            position: "absolute",
+            top: "50%",
+            left: "10%",
+            transform: "translateY(-50%)",
           }}
         >
-          <span style={{ color: "#FFD700", marginRight: "5px" }}>
-            ★ {averageOverallRating}
-          </span>
-          <span style={{ marginLeft: "10px" }}>
-            {totalReviewsCount} Reviews
-          </span>
+          <Image
+            src={image}
+            alt={`${brand} ${name}`}
+            width={460}
+            height={460}
+            style={{
+              objectFit: "contain",
+            }}
+          />
+        </div>
+
+        {/* Text and Review Details on the Right */}
+        <div
+          style={{
+            flex: "1",
+            color: "white",
+            padding: "20px",
+            marginLeft: "auto",
+            textAlign: "left",
+            position: "absolute",
+            right: "10%",
+          }}
+        >
+          <h1 style={{ margin: 0, fontSize: "24px" }}>{brand}</h1>
+          <h2 style={{ margin: "10px 0" }}>{name}</h2>
+          <button
+            style={{
+              backgroundColor: "#0096FF",
+              color: "white",
+              border: "none",
+              padding: "10px 20px",
+              borderRadius: "5px",
+              cursor: "pointer",
+            }}
+          >
+            {networkTechnology}
+          </button>
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              marginTop: "10px",
+              fontSize: "16px",
+            }}
+          >
+            <span style={{ color: "#FFD700", marginRight: "5px" }}>
+              ★ {averageOverallRating}
+            </span>
+            <span style={{ marginLeft: "10px" }}>
+              {totalReviewsCount} Reviews
+            </span>
+          </div>
         </div>
       </div>
+      <div
+        style={{
+          height: "250px",
+          width: "100%",
+          background: "#f8f8f8",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      ></div>
     </div>
   );
 };
