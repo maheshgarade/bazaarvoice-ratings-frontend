1c2147331a886d6c25798dcf83baa33118be1c84
maga0721 <mahesh.garade@netcracker.com>
2025-03-16 13:35:45 +0530

    Fixed CSS isues with PhoneSection

--- src/app/components/product-details/PhoneSection.tsx
+++ src/app/components/product-details/PhoneSection.tsx
@@ -66,7 +66,7 @@ const PhoneSection: React.FC<PhoneSectionProps> = ({ data }) => {
           {/* Phone Image on the Left */}
           <div
             style={{
-              marginRight: "25rem",
+              marginRight: "40rem",
             }}
           >
             <Image
@@ -92,42 +92,82 @@ const PhoneSection: React.FC<PhoneSectionProps> = ({ data }) => {
               right: "10%",
             }}
           >
-            <h1 style={{ margin: 0, fontSize: "24px" }}>{brand}</h1>
-            <h2 style={{ margin: "10px 0" }}>{name}</h2>
-            <button
-              style={{
-                backgroundColor: "#0096FF",
-                color: "white",
-                border: "none",
-                padding: "10px 20px",
-                borderRadius: "5px",
-                cursor: "pointer",
-              }}
-            >
-              {networkTechnology}
-            </button>
             <div
               style={{
                 display: "flex",
+                flexDirection: "column",
                 alignItems: "center",
-                marginTop: "10px",
-                fontSize: "16px",
               }}
             >
-              <span style={{ color: "#FFD700", marginRight: "5px" }}>
-                <Rating
-                  value={averageOverallRating}
-                  precision={0.1}
-                  readOnly
-                  sx={{
-                    "& .MuiRating-iconFilled": { color: "#e8733b" },
-                    "& .MuiRating-iconEmpty": { color: "#e8733b" },
-                  }}
-                />
-              </span>
-              <span style={{ marginLeft: "10px" }}>
-                {totalReviewsCount} Reviews
-              </span>
+              <h1
+                style={{
+                  fontFamily:
+                    "Frutiger LT Std 65 Bold, Helvetica, Arial, sans-serif",
+                  fontStyle: "normal",
+                  fontWeight: 600,
+                  fontSize: "24px",
+                  lineHeight: "32px",
+                  textTransform: "uppercase",
+                }}
+              >
+                {brand}
+              </h1>
+              <h2
+                style={{
+                  margin: "8px 0px",
+                  fontFamily:
+                    "Frutiger LT Std 45 Light, Helvetica, Arial, sans-serif",
+                  fontStyle: "normal",
+                  fontWeight: 400,
+                  fontSize: "48px",
+                  lineHeight: "56px",
+                }}
+              >
+                {name}
+              </h2>
+              <button
+                style={{
+                  backgroundColor: "#82dcfa",
+                  color: "#00008c",
+                  border: "none",
+                  borderRadius: "5px",
+                  cursor: "pointer",
+                  fontFamily:
+                    "Frutiger LT Std 65 Bold, Helvetica, Arial, sans-serif",
+                  fontStyle: "normal",
+                  fontWeight: 600,
+                  fontSize: "14px",
+                  lineHeight: "31px",
+                  marginTop: "1px",
+                  textAlign: "center",
+                  padding: "0 20px",
+                }}
+              >
+                {networkTechnology}
+              </button>
+              <div
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  marginTop: "10px",
+                  fontSize: "16px",
+                }}
+              >
+                <span style={{ color: "#FFD700", marginRight: "5px" }}>
+                  <Rating
+                    value={averageOverallRating}
+                    precision={0.1}
+                    readOnly
+                    sx={{
+                      "& .MuiRating-iconFilled": { color: "#e8733b" },
+                      "& .MuiRating-iconEmpty": { color: "#e8733b" },
+                    }}
+                  />
+                </span>
+                <span style={{ marginLeft: "10px" }}>
+                  {totalReviewsCount} Reviews
+                </span>
+              </div>
             </div>
           </div>
         </div>
